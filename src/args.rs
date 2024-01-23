1a4e5381505f4e2dcbab3670dee519fc5e63c455
Bohdan Katsevych <94533356+bkatsevych@users.noreply.github.com>
2024-01-23 22:07:37 +0200

    update(parser): rewrite in derive + extract into another module

---
+++ src/args.rs
@@ -0,0 +1,108 @@
+use clap::Parser;
+use sysinfo::System;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Input workflow file name
+    #[arg(short = 'f', long)]
+    pub workflow_file: String,
+
+    /// Number of maximal parallel tasks
+    #[arg(long, default_value_t = 100)]
+    pub maxjobs: u16,
+
+    /// Keep executing the pipeline as far possibe (not stopping on first failure).
+    #[arg(short, long)]
+    pub keep_going: bool,
+
+    ///Show what you would do
+    #[arg(long)]
+    pub dry_run: bool,
+
+    ///Saves a graph visualization of workflow
+    #[arg(long)]
+    pub visualize_workflow: bool,
+
+    // TO DO
+    // target_labels: Option<Vec<T>>,
+    // target_tasks: Vec<String>,
+    /// Produces a shell script that runs the workflow in serialized manner and quits
+    #[arg(long)]
+    pub produce_script: Option<String>,
+
+    /// Reruns the workflow starting from given task (or pattern). All dependent jobs will be rerun.
+    #[arg(long)]
+    pub rerun_from: Option<String>,
+
+    /// Simply list all tasks by name and quit
+    #[arg(long)]
+    pub list_tasks: bool,
+
+    // Resources
+    /// Read resource estimates from a JSON and apply where possible.
+    #[arg(long)]
+    pub update_resources: Option<String>,
+
+    /// Update reources estimates of task based on finished related tasks
+    #[arg(long)]
+    pub dynamic_resources: bool,
+
+    /// Try to run workflow even though resource limits might underestimate resource needs of some tasks
+    #[arg(long)]
+    pub optimistic_resources: bool,
+
+    #[arg(long, default_value_t = 1)]
+    pub n_backfill: u16,
+
+    /// Set memory limit as scheduling constraint (in MB)
+    #[arg(long, default_value_t = default_mem_limit())]
+    pub mem_limit: u16,
+
+    /// Set CPU limit (core count)
+    #[arg(long, default_value_t = 8)]
+    pub cpu_limit: u16,
+
+    #[arg(
+        long,
+        help = "Execute pipeline under a given cgroup (e.g., 8coregrid) emulating resource constraints.\nThis must exist and the tasks file must be writable to with the current user."
+    )]
+    pub cgroup: Option<String>,
+
+    // run control, webhooks
+    /// Print log files of failing tasks to stdout,
+    #[arg(long)]
+    pub stdout_on_failure: bool,
+
+    #[arg(long, hide = true)]
+    pub webhook: Option<String>,
+
+    #[arg(long, hide = true)]
+    pub checkpoint_on_failure: Option<String>,
+
+    #[arg(long, hide = true, default_value_t = 0)]
+    pub retry_on_failure: u16,
+
+    #[arg(long, hide = true)]
+    pub no_rootinit_speedup: bool,
+
+    // Logging
+    /// Logfilename for action logs. If none given, pipeline_action_#PID.log will be used
+    #[arg(long)]
+    pub action_logfile: Option<String>,
+
+    /// Logfilename for metric logs. If none given, pipeline_metric_#PID.log will be used
+    #[arg(long)]
+    pub metric_logfile: Option<String>,
+
+    /// Production mode
+    #[arg(long)]
+    pub production_mode: bool,
+}
+
+fn default_mem_limit() -> u16 {
+    let system = System::new_all();
+    let max_system_mem = system.total_memory() as f64;
+
+    (0.9 * max_system_mem / 1024. / 1024.) as u16
+}
--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,10 @@
-use clap::{arg, value_parser, ArgAction, Command};
-use sysinfo::System;
+mod args;
+
+use args::Cli;
+use clap::Parser;
 
 fn main() {
-    let system = System::new_all();
-    let max_system_mem = system.total_memory() as f64;
+    let cli = Cli::parse();
 
-    // defining command line options
-    let matches = Command::new("parser")
-        .about("Parallel execution of a (O2-DPG) DAG data/job pipeline under resource contraints")
-        .arg(
-            arg!(
-                -f --"workflow-file" <FILE> "Input workflow file name."
-            )
-            .required(true),
-        )
-        .arg(
-            arg!(
-                --"maxjobs" [MAX_JOBS] "Number of maximal parallel tasks."
-            )
-            .value_parser(value_parser!(u16))
-            .default_value("100")
-        )
-        .arg(
-            arg!(
-                -k --"keep-going" "Keep executing the pipeline as far possibe (not stopping on first failure)."
-            )
-            .action(ArgAction::SetTrue),
-        )
-        .arg(arg!(--"dry-run" "Show what you would do.").action(ArgAction::SetTrue))
-        .arg(arg!(--"visualize-workflow" "Saves a graph visualization of workflow.")
-            .action(ArgAction::SetTrue))
-        .arg(arg!(--"target-labels" "Runs the pipeline by target labels (example \"TPC\" or \"DIGI\").\nThis condition is used as logical AND together with --target-tasks.")
-            .action(ArgAction::Append)
-            .default_value("")
-        )
-        .arg(arg!(--"target-tasks" "Runs the pipeline by target tasks (example \"tpcdigi\").\nBy default everything in the graph is run. Regular expressions supported.")
-            .action(ArgAction::Append)
-            .default_value("*")
-        )
-        .arg(arg!(--"produce-script" "Produces a shell script that runs the workflow in serialized manner and quits."))
-        .arg(arg!(--"rerun-from" "Reruns the workflow starting from given task (or pattern). All dependent jobs will be rerun."))
-        .arg(arg!(--"list-tasks" "Simply list all tasks by name and quit.")
-            .action(ArgAction::Append))
-
-        // resources
-        .arg(arg!(--"update-resources" "Read resource estimates from a JSON and apply where possible."))
-        .arg(arg!(--"dynamic-resources" "Update reources estimates of task based on finished related tasks")
-            .action(ArgAction::Append))
-        .arg(arg!(--"optimistic-resources" "Try to run workflow even though resource limits might underestimate resource needs of some tasks")
-            .action(ArgAction::Append))
-        .arg(arg!(--"n-backfill" [BACKFILL])
-            .value_parser(value_parser!(u16))
-            .default_value("1")
-        )
-        .arg(arg!(--"mem-limit" [MEM_LIMIT] "Set memory limit as scheduling constraint (in MB)")
-            .value_parser(value_parser!(f64))
-            .default_value((0.9 * max_system_mem / 1024. / 1024.).to_string())
-        )
-        .arg(arg!(--"cpu-limit" [CPU_LIMIT] "Set CPU limit (core count)")
-            .value_parser(value_parser!(u16))
-            .default_value("8")
-        )
-        .arg(arg!(--"cgroup" "Execute pipeline under a given cgroup (e.g., 8coregrid) emulating resource constraints.\nThis must exist and the tasks file must be writable to with the current user."))
-        // run control, webhooks
-        .arg(arg!(--"stdout-on-failure" "Print log files of failing tasks to stdout")
-            .action(ArgAction::SetTrue))
-        .arg(arg!(--"webhook")
-            .hide(true))
-        .arg(arg!(--"checkpoint-on-failure")
-            .hide(true))
-        .arg(arg!(--"retry-on-failure" [RETRY_ON_FAILURE])
-            .value_parser(value_parser!(u16))
-            .default_value("0")
-            .hide(true)
-        )
-        .arg(arg!(--"no-rootinit-speedup")
-            .action(ArgAction::SetTrue)
-            .hide(true))
-        // Logging
-        .arg(arg!(--"action-logfile" "Logfilename for action logs. If none given, pipeline_action_#PID.log will be used"))
-        .arg(arg!(--"metric-logfile" "Logfilename for metric logs. If none given, pipeline_metric_#PID.log will be used"))
-        .arg(arg!(--"production-mode" "Production mode")
-            .action(ArgAction::SetTrue))
-        .get_matches();
-
-    println!("{:?}", matches)
+    println!("{:?}", cli);
 }
