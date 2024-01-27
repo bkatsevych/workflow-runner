6ebf17700b02b073433f21f10ce7f6e825c84bfc
Bohdan Katsevych <94533356+bkatsevych@users.noreply.github.com>
2024-01-27 20:50:00 +0200

    add: logger

---
+++ src/logger.rs
@@ -0,0 +1,50 @@
+use chrono::{DateTime, Local};
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
+#[derive(Debug)]
+pub enum LogLevel {
+    INFO,
+    DEBUG,
+    WARNING,
+}
+
+pub struct Logger {
+    log_file: String,
+    level: LogLevel,
+}
+
+impl Logger {
+    pub fn new(log_file: String, level: LogLevel) -> Logger {
+        Logger { log_file, level }
+    }
+
+    pub fn log(&self, level: &LogLevel, message: &str) {
+        let now: DateTime<Local> = Local::now();
+        let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
+        let log_message = format!("{:?} {:?} {}\n", formatted_time, level, message);
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(&self.log_file)
+            .unwrap();
+
+        if let Err(e) = writeln!(file, "{}", log_message) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+    }
+
+    pub fn info(&self, message: &str) {
+        self.log(&LogLevel::INFO, message);
+    }
+
+    pub fn debug(&self, message: &str) {
+        self.log(&LogLevel::DEBUG, message);
+    }
+
+    pub fn warning(&self, message: &str) {
+        self.log(&LogLevel::WARNING, message);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use args::Args;
 use clap::Parser;
+use std::process;
+
+use crate::logger::{LogLevel, Logger};
 
 mod args;
+mod logger;
 mod resource_manager;
 mod utils;
 mod workflow_executor;
@@ -11,4 +15,19 @@ fn main() {
     let args = Args::parse();
 
     println!("{:?}", args);
+
+    let actionlogger = Logger::new(
+        format!("pipeline_action_{}.log", process::id()),
+        LogLevel::INFO,
+    );
+
+    let metriclogger = Logger::new(
+        format!("pipeline_metric_{}.log", process::id()),
+        LogLevel::INFO,
+    );
+
+    // actionlogger.info("Score for task is 10");
+    // metriclogger.info("Resources per task is 5");
+    // actionlogger.debug("Task has not enough points (< 3) to sample resources, setting to previously assigned values.");
+    // actionlogger.warning("Sampled CPU (2.5) exceeds assigned CPU limit (2.0)");
 }
